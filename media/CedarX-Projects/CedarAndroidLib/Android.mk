e9429d26b3a024c9434de9f5f79302af75f5a09a
CorvusMod <corvusmod@home>
2012-10-23 12:26:08 +0200

    Delete all
    Change-Id: I9deb8e5073f9df66dca151bdbf93c5d8ca610a60

--- media/CedarX-Projects/CedarAndroidLib/Android.mk
+++
@@ -1,4 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-
-include $(TOP)/frameworks/av/media/CedarX-Projects/Config.mk
-include $(LOCAL_PATH)/LIB_$(CEDARX_ANDROID_CODE)_F23/Android.mk
--- media/CedarX-Projects/CedarXAndroid/Android.mk
+++
@@ -1,5 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-include $(LOCAL_PATH)/../Config.mk
-include $(LOCAL_PATH)/IceCreamSandwich/Android.mk
